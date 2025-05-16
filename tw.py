1d063fbcd76767d35d5efef165a72332a74b2378
Aruj Anand <arujanand2@gmail.com>
2025-05-16 11:01:04 +0530

    Added day persistence

--- tw.py
+++ tw.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 import tweepy
 import sys
+from datetime import date
+import json
 
 load_dotenv()
 
@@ -44,8 +46,35 @@ def post_tweet(day_count, image_path, tags_list):
     url=get_url(response.data['text'])
     print(f"Tweet posted! ID: {response.data['id']}\nView Tweet: {url}")
 
+# set day accoding to date
+DAY_FILE="day_data.json"
+def load_day():
+   current_date=date.today()
+
+   #if ran for first time
+   if not os.path.isfile(DAY_FILE):
+    data={"day":1,"last_posted":current_date.isoformat()}
+    with open(DAY_FILE,"w") as my_file:
+        json.dump(data,my_file)
+    return 1
+   
+   #if ran previously than add day
+   with open(DAY_FILE,"r") as my_file:
+      data=json.load(my_file)
+      last_date=date.fromisoformat(data["last_posted"])
+    
+   days_passed=(current_date-last_date).days
+
+   if (days_passed > 0):
+       day_today=data["day"]+days_passed
+       data={"day":day_today,"last_posted":current_date.isoformat()}
+       with open(DAY_FILE,"w") as my_file:
+           json.dump(data,my_file)
+
+   return(day_today)
+
 if __name__ == "__main__":
-    day = 41
+    day = load_day()
 
     if len(sys.argv) > 1:
         image_path=sys.argv[1]
@@ -55,4 +84,4 @@ if __name__ == "__main__":
         sys.exit(1)
     tags_to_mention = ["geeksforgeeks"]
 
-    post_tweet(day, image_path, tags_to_mention)
+    # post_tweet(day, image_path, tags_to_mention)
